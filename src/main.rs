c11e18fc8951b028041dc040539c3bbf0342811e
fys <1113014250@qq.com>
2022-03-02 22:58:53 +0800

    simple mini-rust-async-runtime done

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
+use crossbeam::channel;
+use futures::{
+    task::{self, ArcWake},
+    Future,
+};
 use std::{
     pin::Pin,
     sync::{Arc, Mutex},
+    task::{Context, Poll},
+    time::{Duration, Instant},
 };
 
-use crossbeam::channel;
-use futures::Future;
-
 fn main() {
-    println!("hello mini-rust-runtime!")
+    let mini_rust = MiniRust::new();
+    mini_rust.spawn(async {
+        Delay {
+            when: Instant::now() + Duration::from_secs(5),
+        }
+        .await;
+        println!("hello mini-rust-runtime!");
+    });
+    mini_rust.spawn(async {
+        Delay {
+            when: Instant::now() + Duration::from_secs(10),
+        }
+        .await;
+        println!("hello fys!");
+    });
+    mini_rust.run();
 }
 
 struct MiniRust {
@@ -24,8 +43,15 @@ impl MiniRust {
 
     fn spawn<F>(&self, future: F)
     where
-        F: Future<Output = ()> + Send,
+        F: Future<Output = ()> + Send + 'static,
     {
+        Task::spawn(future, &self.sender);
+    }
+
+    fn run(&self) {
+        while let Ok(task) = self.scheduled.recv() {
+            task.poll();
+        }
     }
 }
 
@@ -51,7 +77,7 @@ impl Task {
         //     future,
         //     executor: sender.clone(),
         // });
-        // 
+        //
         // A: Because future is not trait object, need convert to trait object
         let future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>> = Mutex::new(Box::pin(future));
         let task = Arc::new(Task {
@@ -63,5 +89,56 @@ impl Task {
             executor: sender.clone(),
         });
         let _ = sender.send(task);
+    }
+
+    fn poll(self: Arc<Self>) {
+        // Get a waker referencing the task
+        let waker = task::waker(self.clone());
+
+        // Initialize the task context with the waker
+        let mut cx = Context::from_waker(&waker);
+
+        // This will never block as only a single thread
+        let mut future = self.future.try_lock().unwrap();
+
+        // Poll the future
+        let _ = future.as_mut().poll(&mut cx);
+    }
+}
+
+// Q: how to work?
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        // Push task to executor channel.
+        // The executor recvices from the executor channel and polls tasks
+        let _ = arc_self.executor.send(arc_self.clone());
+    }
+}
+
+struct Delay {
+    // when to complete the delay
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = ();
+
+    // Q: why use Pin?
+    // Q: the memory layout of &str and String
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> task::Poll<Self::Output> {
+        let now = Instant::now();
+        if now >= self.when {
+            return Poll::Ready(());
+        }
+
+        let when = self.when;
+        let wake = Arc::new(Mutex::new(cx.waker().clone()));
+        std::thread::spawn(move || {
+            std::thread::sleep(when - now);
+            let waker = wake.lock().unwrap();
+            waker.wake_by_ref();
+        });
+
+        Poll::Pending
     }
 }
